feac023595bd48554a5652a0d092e53c68809100
sandymaramma <edesandraoge@gmail.com>
2023-08-09 23:08:54 +0000

    0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- SQL script that creates a tabl:users
 -- script can be executed on any database
-CREATE TABLE IF NOT EXIST 'users' (
-	'id' INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	'email' VARCHAR (255) NOT NULL UNIQUE,
-	'name' VARCHAR (255)
+CREATE TABLE If NOT EXISTS `users` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255)
 );

fb0d02932c37834a6f9397468735aa745e24f07f
sandymaramma <edesandraoge@gmail.com>
2023-08-14 22:30:30 +0100

    8-all.py

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" 
+Python function that lists all documents in a collection
+
+Prototype: def list_all(mongo_collection)
+
+Return an empty list if no document in the collection
+
+mongo_collection will be the pymongo collection object
+
+"""
+def list_all(mongo_collection):
+    documents = mongo_collection.find()
+
+    if documents.count() == 0:
+        return []
+    
+    return documents
